307006b4619c00dbebafa59d7147109b58101713
gipark <gipark@student.42seoul.kr>
2020-10-11 20:52:26 +0900

    [BOJ_10872]_팩토리얼

--- 10800-10899/10872.c
+++ 10800-10899/10872.c
@@ -1,14 +1,33 @@
 #include <stdio.h>
 
+// int main(void)
+// {
+//     int n;
+//     int factorial;
+//
+//     scanf("%d", &n);
+//     factorial = 1;
+//     for (int i = 1; i <= n; i++)
+//         factorial *= i;
+//     printf("%d\n", factorial);
+//     return (0);
+// }
+
+int factorial(int n)
+{
+    if (n == 0)
+        return (1);
+    else if (n >= 1)
+        return (n * factorial(n - 1));
+    else
+        return (0);
+}
+
 int main(void)
 {
     int n;
-    int factorial;
 
     scanf("%d", &n);
-    factorial = 1;
-    for (int i = 1; i <= n; i++)
-        factorial *= i;
-    printf("%d\n", factorial);
+    printf("%d\n", factorial(n));
     return (0);
 }
